9c040b7fef6643f15cab9950bb8485c79076eedc
しおり.大隅 <metalgear.shiorid@gmail.com>
2021-09-30 17:45:34 +0900

    imgと30文字以上の時、質問

--- index.php
+++ index.php
@@ -25,8 +25,8 @@
                 <div class="post-flex-container-img">
                   <?php if (has_post_thumbnail()) : ?>
                     <img class="post-flex-container-img-image">
-                      <?php the_post_thumbnail('post-thumbnails', array('width' => '100%', 'height' => '180px',  'alt' => the_title_attribute('echo=0'))); ?> 
-                      <!-- <img class="post-flex-container-img-image" alt="アイキャッチ画像がありません"> -->
+                    <?php the_post_thumbnail('post-thumbnails', array('width' => '100%', 'height' => '180px',  'alt' => the_title_attribute('echo=0'))); ?>
+                    <!-- <img class="post-flex-container-img-image" alt="アイキャッチ画像がありません"> -->
                     <!-- <img src="./img/blog__first.jpg" class="post-flex-container-img-image"  alt=""> -->
                   <?php else : ?>
                     <img class="post-flex-container-img-image" src="<?php echo get_template_directory_uri(); ?>/img/no-images.png" alt="no-img">
@@ -36,9 +36,9 @@
                   <h3 class="post-flex-container-desc-title">
                     <?php echo get_the_title(); ?>
                     <?php
-                    if (mb_strlen($post->post_title) > 30) {
-                      $title = mb_substr($post->post_title, 0, 30);
-                      echo $title . "...";
+                    if (mb_strlen($post->post_title, 'UTF-8') > 30) {
+                      $title = mb_substr($post->post_title, 0, 30, 'UTF-8');
+                      echo $title . '…';
                     } else {
                       echo $post->post_title;
                     }
--- single.php
+++ single.php
@@ -1,35 +1,35 @@
 <?php get_header(); ?>
 
 <main class="inner">
-<?php if (have_posts()) : the_post(); ?>
-  <div class="article">
-    <!-- <div href="" class="article-category">カテゴリーの名前</div> -->
-    <?php if (has_category()) : ?>
-      <div class="article-category"><?php echo get_the_category_list(' '); ?>
-      <?php endif; ?>
-    </div>
+  <?php if (have_posts()) : the_post(); ?>
+      <div class="article">
+        <!-- <div href="" class="article-category">カテゴリーの名前</div> -->
+        <?php if (has_category()) : ?>
+          <div class="article-category"><?php echo get_the_category_list(' '); ?>
+          <?php endif; ?>
+          </div>
 
-    <!-- <h1 class="article-title">記事のタイトル</h1> -->
-    <h1 class="article-title"><?php the_title(); ?></h1>
+          <!-- <h1 class="article-title">記事のタイトル</h1> -->
+          <h1 class="article-title"><?php the_title(); ?></h1>
 
-    <!-- <p class="article-date">2021.02.26</p> -->
-    <p class="article-date"><?php echo get_the_date('Y.m.d'); ?></p>
+          <!-- <p class="article-date">2021.02.26</p> -->
+          <p class="article-date"><?php echo get_the_date('Y.m.d'); ?></p>
 
-    <div class="article-img">
-      <?php if (has_post_thumbnail()) : ?>
-        <?php the_post_thumbnail('post-thumbnails', array('width' => '100%', 'alt' => the_title_attribute('echo=0'))); ?>
+          <div class="article-img">
+            <?php if (has_post_thumbnail()) : ?>
+              <?php the_post_thumbnail('post-thumbnails', array('width' => '100%', 'alt' => the_title_attribute('echo=0'))); ?>
+            <?php else : ?>
+              <img src="<?php echo get_template_directory_uri(); ?>/img/no-images.png" width="100%" alt="no-img">
+            <?php endif; ?>
+          </div>
+
+          <div class="article-text">
+            <?php the_content(); ?>
+          </div>
+
       <?php else : ?>
-        <img src="<?php echo get_template_directory_uri(); ?>/img/no-images.png" width="100%" alt="no-img">
+        <p class="no-see">投稿が見つかりません～</p>
       <?php endif; ?>
-    </div>
-
-    <div class="article-text">
-      <?php the_content(); ?>
-    </div>
-
-      <?php endelse: ?>
-      <p class="no-see">投稿が見つかりません</p>
-      <?php endif; ?>
-  </div>
+      </div>
 </main>
-  <?php get_footer(); ?>
+<?php get_footer(); ?>
